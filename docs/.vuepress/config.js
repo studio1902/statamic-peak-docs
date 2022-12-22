51ff1ccc64206a1894f16468d9de4935c79a28ae
Rob de Kort <rob@studio1902.nl>
2022-12-22 15:40:36 +0100

    Fix docs link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,8 +16,8 @@ module.exports = {
     title: 'Statamic Peak Docs',
     themeConfig: {
       docsBranch: 'main',
-      docsRepo: 'studio1902/statamic-peak',
-      docsDir: 'docs/docs',
+      docsRepo: 'studio1902/statamic-peak-docs',
+      docsDir: 'docs',
       editLinks: true,
       editLinkText: 'Improve this page on GitHub',
       lastUpdated: true,
